8a6ce9db7f22c15e182563ba953bd65c3fccee70
Chandrahasa Pranava Vemu <38093314+cpranava@users.noreply.github.com>
2020-06-15 09:15:48 +0800

    Update append-survey.py

--- append-survey.py
+++ append-survey.py
@@ -8,7 +8,7 @@ ANSIBLE_USERNAME=sys.argv[3]
 ANSIBLE_PASSWORD=sys.argv[4]
 
 #Load
-finallist = list(sys.argv[1].replace('[','').replace(']','').replace(',','')) 
+finallist = list(sys.argv[1].replace('[','').replace(']','').split(','))
 final_list_string = "\n".join(finallist)
 
 #Initialize json
@@ -16,12 +16,9 @@ json_survey_string = '{ "description": "", "name": "", "spec": [] }'
 json_survey = json.loads(json_survey_string)
 
 #Create final survey value
-json_survey["spec"].append({"question_description": "Select the list of VMs to be deleted", "default": "", "min":None, "max":None, "choices": final_list_string.replace('\\n', '\n'), "variable": "vm_name", "question_name": "VM names", "type": "multiplechoice", "required": True})
+json_survey["spec"].append({"question_description": "Select the list of VMs to be deleted", "default": "", "min": None, "max": None, "choices": final_list_string.replace('\\n', '\n'), "variable": "vm_name", "question_name": "VM names", "type": "multiselect", "required": True})
 
 headers = {
     'content-type': 'application/json',
 }
-
 response = requests.post('https://{}/api/v2/job_templates/{}/survey_spec/'.format(ANSIBLE_HOST_IP, ANSIBLE_JOB_TEMPLATE), headers=headers, data=json.dumps(json_survey), verify=False, auth=(ANSIBLE_USERNAME, ANSIBLE_PASSWORD))
-
-
